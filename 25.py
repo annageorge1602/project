368d0e5689594d2481f3f3cddad93905f806d51b
anna <anna.inmca2025@saintgits.org>
2024-05-20 15:38:36 +0530

    first commit

---
+++ 25.py
@@ -0,0 +1,18 @@
+a=int(input('enter the first no:'))
+
+b=int(input('enter the second no:'))
+
+c=int(input('enter the third no:'))
+
+def maximum(a, b, c):
+ 
+    if (a >= b) and (a >= c):
+        largest = a
+ 
+    elif (b >= a) and (b >= c):
+        largest = b
+    else:
+        largest = c
+         
+    return largest
+print(maximum(a, b, c))
